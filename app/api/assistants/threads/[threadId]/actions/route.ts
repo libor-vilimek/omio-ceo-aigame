4064a6b407f0e02b44e8d936ea96213d5b2dc032
liborvilimekomio <libor.vilimek@omio.com>
2025-06-19 16:39:03 +0200

    make it working

--- app/api/assistants/threads/[threadId]/actions/route.ts
+++ app/api/assistants/threads/[threadId]/actions/route.ts
@@ -1,15 +1,18 @@
-import { openai } from "@/app/openai";
+import {openai} from "@/app/openai";
 
 // Send a new message to a thread
-export async function POST(request, { params: { threadId } }) {
-  const { toolCallOutputs, runId } = await request.json();
+export async function POST(request, {params: {threadId}}) {
+    const {toolCallOutputs, runId} = await request.json();
 
-  const stream = openai.beta.threads.runs.submitToolOutputsStream(
-    threadId,
-    runId,
-    // { tool_outputs: [{ output: result, tool_call_id: toolCallId }] },
-    { tool_outputs: toolCallOutputs }
-  );
+    const stream = openai.beta.threads.runs.submitToolOutputsStream(
+        runId,
+        {
+            thread_id: threadId,
+            tool_outputs: toolCallOutputs
+        }
+        // { tool_outputs: [{ output: result, tool_call_id: toolCallId }] },
+        // { tool_outputs: toolCallOutputs }
+    );
 
-  return new Response(stream.toReadableStream());
+    return new Response(stream.toReadableStream());
 }
